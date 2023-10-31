747515baa5b59e21f90d26ab35b96431acc1904c
bassem <bassemgog@gmail.com>
2023-10-31 22:01:18 +0200

    fg

--- src/app/authentication/register/register.component.ts
+++ src/app/authentication/register/register.component.ts
@@ -24,79 +24,63 @@ import { RecaptchaModule } from 'ng-recaptcha/lib/recaptcha.module';
 
 import { AbstractControl, ValidatorFn } from '@angular/forms';
 
-import {  ValidationErrors } from '@angular/forms';
+import { ValidationErrors } from '@angular/forms';
 import { of, Observable } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
 import { matchPasswordAsync } from './confirm-password';
 
-
-
-
-
-
-
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
-
 })
 export class RegisterComponent implements OnInit {
   public countries: any = countries;
   ReCaptchaV3Service: any;
   // siteKey: string | undefined;
   // this.siteKey = "your-actual-site-key";
-siteKey = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
+  siteKey = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
 
+  // ------------ one captcha section?++++++++++++++++++++++++++++
+  captcha: string;
+  email: string;
+  public isCaptchaClicked: boolean = false;
 
-// ------------ one captcha section?++++++++++++++++++++++++++++
-captcha:string;
-email:string;
-public isCaptchaClicked : boolean= false;
+  resolved(captchaResponse: string) {
+    this.captcha = captchaResponse;
+    console.log('resolved witht the response' + captchaResponse);
+    this.isCaptchaClicked = true;
+  }
 
-resolved(captchaResponse:string){
-  this.captcha=captchaResponse;
-  console.log("resolved witht the response"+ captchaResponse);
-  this.isCaptchaClicked = true;
+  isCaptchaValidated = false;
 
-}
+  //   @ViewChild(ReCaptchaComponent)
+  //   captcha!: ReCaptchaComponent;
 
- isCaptchaValidated = false;
+  // onCaptchaResponse(event: any) {
+  //   this.isCaptchaValidated = true;
+  // }
 
+  // ------------?+++++ confirm password section+++++++++++++++++++++++
+  // name = 'Angular ' + VERSION.major;
+  //   passwordsMatching = false;
+  //   isConfirmPasswordDirty = false;
+  //   confirmPasswordClass = 'form-control';
+  //   newPassword = new FormControl(null, [
+  //     (c: AbstractControl) => Validators.required(c),
+  //     Validators.pattern(
+  //       /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+  //     ),
+  //   ]);
+  //   confirmPassword = new FormControl(null, [
+  //     (c: AbstractControl) => Validators.required(c),
+  //     Validators.pattern(
+  //       /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
+  //     ),
+  //   ]);
 
-
-//   @ViewChild(ReCaptchaComponent)
-//   captcha!: ReCaptchaComponent;
-
-// onCaptchaResponse(event: any) {
-//   this.isCaptchaValidated = true;
-// }
-
-// ------------?+++++ confirm password section+++++++++++++++++++++++
-// name = 'Angular ' + VERSION.major;
-//   passwordsMatching = false;
-//   isConfirmPasswordDirty = false;
-//   confirmPasswordClass = 'form-control';
-//   newPassword = new FormControl(null, [
-//     (c: AbstractControl) => Validators.required(c),
-//     Validators.pattern(
-//       /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
-//     ),
-//   ]);
-//   confirmPassword = new FormControl(null, [
-//     (c: AbstractControl) => Validators.required(c),
-//     Validators.pattern(
-//       /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@$!%*#?&^_-]).{8,}/
-//     ),
-//   ]);
-
-
-
-
-
-
-ngOnInit(): void {
+  ngOnInit(): void {
     const inputElement = document.querySelector('#phone');
     if (inputElement) {
       intlTelInput(inputElement, {
@@ -115,8 +99,7 @@ ngOnInit(): void {
     return password === confirmPassword ? null : { mismatch: true };
   }
 
-
-  checkoutForm: FormGroup;
+  registerForm: FormGroup;
 
   constructor(
     private _snackBar: MatSnackBar,
@@ -124,57 +107,70 @@ ngOnInit(): void {
     private formBuilder: FormBuilder,
     private router: Router
   ) {
-
     this.captcha = '';
-    this.email='aabassadkfdsafdkajskdj';
+    this.email = 'aabassadkfdsafdkajskdj';
 
-
-    this.checkoutForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: [
-        '',
-        [
-          Validators.required,
-          Validators.email,
-          Validators.pattern(
-            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
-          ),
+    this.registerForm = this.formBuilder.group(
+      {
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.email
+            // ,
+            // Validators.pattern(
+            //   /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+            // ),
+          ],
         ],
-      ],
-      country: ['', Validators.required],
-      phone: ['', Validators.required],
-      password:['',Validators.required],
-      // confirmPassword: ['',  [Validators.required, this.passwordsMatchValidator]]
-      confirmPassword: ['', [Validators.required], matchPasswordAsync]
+        country: ['', Validators.required],
+        // phone: ['', Validators.required],
+        phone: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              /^\d{10}$/ // Adjust the pattern as needed for your phone number format
+            ),
+          ],
+        ],
+        // password: ['', Validators.required],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(8),
+            Validators.pattern(
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#\$%\^&\*\(\)_\+{}\[\]:;<>,\.?~\\`/-])/
+            ),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required], matchPasswordAsync],
+        companyName: ['', Validators.required],
+        userType: ['', Validators.required],
+        userRole: ['', Validators.required],
+        recaptcha: ['', [Validators.required]],
+        address: ['', Validators.required],
+        tel: ['', Validators.required],
 
-      ,
-      companyName: [''],
-      userType: ['', Validators.required],
-      userRole: ['', Validators.required],
-      // email:['',Validators.email],
-      recaptcha: ['', [Validators.required]],
-      address: ['', Validators.required],
-      tel: ['', Validators.required],
-      city: [''],
-      state: [''],
-      zip: [''],
-      message: [''],
-      // recaptcha: ['', [Validators.required], [this.ReCaptchaV3Service.validate()]]
-    },{
-      validator: this.ConfirmedValidator('password', 'confirmPassword')
-    });
-
-
-
+        // recaptcha: ['', [Validators.required], [this.ReCaptchaV3Service.validate()]]
+      },
+      {
+        validator: this.ConfirmedValidator('password', 'confirmPassword'),
+      }
+    );
   }
-
 
   ConfirmedValidator(controlName: string, matchingControlName: string) {
     return (formGroup: FormGroup) => {
       const control = formGroup.controls[controlName];
       const matchingControl = formGroup.controls[matchingControlName];
-      if (matchingControl.errors && !matchingControl.errors['confirmedValidator']) {
+      if (
+        matchingControl.errors &&
+        !matchingControl.errors['confirmedValidator']
+      ) {
         return;
       }
       if (control.value !== matchingControl.value) {
@@ -188,17 +184,17 @@ ngOnInit(): void {
   onSubmit(): void {
     console.log('Submit');
 
-    console.log(this.checkoutForm.value);
-    if (this.checkoutForm.valid) {
-    const order = {
-      order_status: 'pending',
-      customer_name:
-        this.checkoutForm.value.firstName + this.checkoutForm.value.lastName,
-      customer_phone: this.checkoutForm.value.phone,
-      customer_email: this.checkoutForm.value.email,
-      customer_address: this.checkoutForm.value.address,
-    };
-  }
+    console.log(this.registerForm.value);
+    if (this.registerForm.valid) {
+      const order = {
+        order_status: 'pending',
+        customer_name:
+          this.registerForm.value.firstName + this.registerForm.value.lastName,
+        customer_phone: this.registerForm.value.phone,
+        customer_email: this.registerForm.value.email,
+        customer_address: this.registerForm.value.address,
+      };
+    }
   }
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action);
@@ -230,5 +226,20 @@ ngOnInit(): void {
 
   selectedValue: string | undefined;
   selectedCar: string | undefined;
+  // hasErrors(): any {
+  //   return (
+  //     this.registerForm.get('username')?.invalid ||
+  //     this.registerForm.get('email')?.invalid ||
+  //     this.registerForm.get('password')?.invalid
+  //   );
+  // }
+  hasErrors(): any {
+    for (const controlName in this.registerForm.controls) {
+      if (this.registerForm.get(controlName)?.invalid) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 }
